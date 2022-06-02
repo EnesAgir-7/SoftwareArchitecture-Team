48a51cc141bb7802353b5190f821ca335a547d1e
Enes AGIR <enes.agir.7@gmail.com>
2022-06-02 22:45:30 +0200

    Update messagesController.js

--- server/controllers/messagesController.js
+++ server/controllers/messagesController.js
@@ -1,9 +1,23 @@
 const Messages = require("../models/messageModel");
 
-module.exports.getMessages = async (req, res, next) => {
-    
+module.exports.addMessage = async (req, res, next) => {
+    try {
+        const {from,to,messages} =req.body;
+        const data = await MessageModel.create({
+            message:{text:message},
+            users:[from,to],
+            //^ sequence 
+            sender:from,
+        });
+        if(data) return res.json({message:"Message added/saved successfully..."});
+
+    return res.json({message:"Message failed save to DB"});
+
+    } catch (err) {
+        next(err);
+    }
 };
 
-module.exports.addMessage = async (req, res, next) => {
+module.exports.getMessages = async (req, res, next) => {
     
 };
