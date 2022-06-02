6baa077fd35ed17f800ac047cdb86f1018d82716
Enes AGIR <enes.agir.7@gmail.com>
2022-06-02 22:23:31 +0200

    create message controller

---
+++ server/controllers/messagesController.js
@@ -0,0 +1,9 @@
+const Messages = require("../models/messageModel");
+
+module.exports.getMessages = async (req, res, next) => {
+    
+};
+
+module.exports.addMessage = async (req, res, next) => {
+    
+};
