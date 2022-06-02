a6b4db54805e40d7b580d0d28ac9921a19d2e650
Enes AGIR <enes.agir.7@gmail.com>
2022-06-02 22:17:49 +0200

    message Model
    for backend

--- public/src/components/ChatContainer.jsx
+++ public/src/components/ChatContainer.jsx
@@ -18,7 +18,7 @@ return (
         <div className="chat-header">
             <div className="user-details">
                 <div className="avatar">
-                    <img src={`data:image/svg+xml;base64,${currentChat.avatarImage}`} alt="" />
+                    <img src={`data:image/svg+xml;base64,${currentChat.avatarImage}`} alt="avatar" />
                 </div>
                 <div className="username">
                     <h3>{currentChat.username}</h3>
